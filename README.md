f38c69de5f4fe30d852da1b36e69e6af6a96e114
Juan Barberis <juancebarberis@gmail.com>
2020-06-12 19:57:35 -0300

    Added more prints with info. Better DROP DATABASE

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Script for a single MySQL database sync from a SFTP server
 
+This script was made for a very specific use case. It basically connects to an SFTP server, download a SQL file from a specific folder and 'dump' it to a MySQL database.
+
 ## Requirements
 
 This script was made for Windows Server in mind. You can adapt it to use on Linux or MacOS, should be easy.
 
-Python version 3
+Python version 3.
 
 Python dependencies:
   - pysftp
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from env import *
 import pysftp
 import os
-import pymysql
+import pymysql.cursors
 
 def get_file():
   '''
   Download the SQL file from the remote SFTP server
   '''
+  print("")
   cnopts = pysftp.CnOpts()
   cnopts.hostkeys = None 
-  print("")
   with pysftp.Connection(HOST, username=USER, password=PASSWORD, cnopts=cnopts) as sftp:
       print(f"Connection succesfully stablished with {HOST} ({USER})")
       with sftp.cd(DIR):  
@@ -18,25 +18,38 @@ def get_file():
           sftp.get(REMOTE_FILE, "out/output.sql")         
           print(f"Download complete.")
 
-def create_database():
+def reload_database():
   '''
   Create the database if not exists
   '''
+  print("")
   db = pymysql.connect(LOCAL_DB_HOST, LOCAL_DB_USER, LOCAL_DB_PASSWORD)
   cursor = db.cursor()
+  cursor.execute("USE information_schema")
+  cursor.execute('SELECT COUNT(*) FROM schemata WHERE SCHEMA_NAME=%s', (LOCAL_DB_NAME))
+  result = cursor.fetchone()
+  
+  if(result[0] >= 1):
+    print("The database exists.\nExecuting DROP DATABASE...")
+    cursor.execute("DROP DATABASE "+ LOCAL_DB_NAME +";")
+  else:
+    print("The database does not exists. Trying to create...")
 
-  drop = cursor.execute("CREATE DATABASE IF NOT EXISTS "+ LOCAL_DB_NAME +";")
+  cursor.execute("CREATE DATABASE "+ LOCAL_DB_NAME)
+  print(f"Database {LOCAL_DB_NAME} created")
   
   db.close()
 
   
 def dump():
+  # Remove .lnk y you're Windows terminal recognize it in a different way.
+  print("")
   os.system('cmd /k "mysqldump.lnk --add-drop-database '+ LOCAL_DB_NAME +' < out/output.sql"') 
 
 
 def main():
   get_file()
-  create_database()
+  reload_database()
   dump()
 
 main()
